cba78b19f14f5c442b6f10821bf5589f8c55ea2f
Rinat Mushakeev <rinat_nova@mail.ru>
2023-02-25 13:29:26 +0300

    практика 2 commit

---
+++ Practika2/zadanie19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+if (number!.Length == 5)
+{
+    // int a1 = Convert.ToInt32(Convert.ToString(number![0]));
+    // int a2 = Convert.ToInt32(Convert.ToString(number![1]));
+    // int a4 = Convert.ToInt32(Convert.ToString(number![3]));
+    // int a5 = Convert.ToInt32(Convert.ToString(number![4]));
+
+    // if ((a1 == a5) && (a2 == a4))
+    if ((number[0] == number[4]) && (number[1] == number[3]))
+    {
+        Console.WriteLine($"Число {number!} Палиндром");       
+    }    
+    else 
+    {
+        Console.WriteLine($"Число {number!} не Палиндром");
+    }
+        
+}
+else
+{
+    System.Console.WriteLine("Число не является пятизначным");
+}
---
+++ Practika2/zadanie23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+System.Console.Write("Введите число N: ");
+int numberN  = Convert.ToInt32(Console.ReadLine());
+if (numberN >= 1)
+{
+    Console.Write($"{numberN} -> ");
+    for (int i = 1; i <= numberN; i++)
+    {
+        System.Console.Write(Math.Pow(i, 3) + ", ");
+    }
+}
+else
+{
+    Console.WriteLine($"Число меньше 1");
+}
