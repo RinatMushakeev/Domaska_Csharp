9a03e4b2a8797d99a3ca2d1058e00d2e9d18fb7d
Rinat Mushakeev <rinat_nova@mail.ru>
2023-03-12 18:42:33 +0300

    практика 47 50 52 commit

---
+++ Practika5/zadanie21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+int[] coordsA = new int[3];
+int[] coordsB = new int[3];
+System.Console.Write("Введите коордитинаты x точки A: ");
+coordsA[0]  = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордитинаты y точки A: ");
+coordsA[1]  = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордитинаты z точки A: ");
+coordsA[2]  = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордитинаты x точки B: ");
+coordsB[0]  = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордитинаты y точки B: ");
+coordsB[1]  = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордитинаты z точки B: ");
+coordsB[2]  = Convert.ToInt32(Console.ReadLine());
+
+int distanceX = (int)Math.Pow(coordsB[0] - coordsA[0], 2);
+int distanceY = (int)Math.Pow(coordsB[1] - coordsA[1], 2);
+int distanceZ = (int)Math.Pow(coordsB[2] - coordsA[2], 2);
+// double distanceY = Math.Pow(coordsB[1] - coordsA[1], 2);
+double Result = Math.Round(Math.Sqrt(distanceX + distanceY + distanceZ), 2);
+
+System.Console.WriteLine(Result);
---
+++ Practika7/zadanie50/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1 7 -> такого числа в массиве нет
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    Random rand = new Random();
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void PositionElement(int[,] matr, int x, int y)
+{
+    if (x >= matr.GetLength(0) || y >= matr.GetLength(1))
+    {
+        System.Console.Write("Такого числа нет ");
+    }
+    else 
+    {
+        System.Console.Write(matr[x,y] + " ");
+    }
+}
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+int x = ReadInt("Введите строку элемента ");
+int y = ReadInt("Введите столбец элемента ");
+PositionElement(myMatrix, x-1, y-1);
